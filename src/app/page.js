a413034e91c4953ee1a4f921f1d4529ce6d59892
Dafne <lire.alarcon.06@gmail.com>
2026-02-28 10:18:16 -0600

    Elimina botones sin funcion

--- src/app/page.js
+++ src/app/page.js
@@ -8,17 +8,6 @@ export default function Home() {
         <h1>Bienvenido a GYM-RAT del TecMilenio</h1>
         <Link href="/instructors"><button type="button" id="instructures"
         className="m-2 border w-60">Instructors</button></Link>
-
-          <button type="button" id="schudule" 
-          className="m-2 border w-60">Horarios del instructor</button>
-          <button type="button" id="rutines"
-          className="m-2 border w-60">Rutinas</button>
-          <button type="button" id="book"
-          className="m-2 border w-60">Bitacora de alumnos</button>
-          <button type="button" id="frequency"
-          className="m-2 border w-60">Grafica de horarios</button>
-          <button type="button" id="inventory"
-          className="m-2 border w-60">Inventario</button>
       </div>
   </div>
   );
--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -4,7 +4,7 @@ export default function Input({ className = '', ...props }) {
       className={[
         'w-full px-3 py-2 rounded-lg border border-slate-200 bg-white',
         'focus:outline-none focus:ring-2 focus:ring-slate-400 focus:border-transparent',
-        'text-black placeholder:text-slate-400 caret-black'
+        'text-black placeholder:text-slate-400 caret-black',
         className
       ].join(' ')}
       {...props}
