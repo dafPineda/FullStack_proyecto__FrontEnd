ab4874d807cd3072431b091696d437a8c576933d
Dafne <lire.alarcon.06@gmail.com>
2026-02-28 10:13:31 -0600

    UI pon letra negra en login

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -3,7 +3,7 @@ export default function Input({ className = '', ...props }) {
     <input 
       className={[
         'w-full px-3 py-2 rounded-lg border border-slate-200 bg-white',
-        'focus:outline-none focus:ring-2 focus:ring-slate-400 focus:border-transparent',
+        'focus:outline-none focus:ring-2 focus:ring-slate-400 focus:border-transparent text-black',
         className
       ].join(' ')}
       {...props}
